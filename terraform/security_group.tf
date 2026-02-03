6cfbe586a73cd9ec5f1da1e2bbcd3c70bd84ed8d
JyothiKumar37 <jyothikumarreddy2005@gmail.com>
2026-02-03 16:19:09 +0530

    Create security_group.tf

---
+++ terraform/security_group.tf
@@ -0,0 +1,24 @@
+resource "aws_security_group" "ec2_sg" {
+  name        = "terraform-ec2-sg"
+  description = "Allow SSH access"
+
+  ingress {
+    description = "SSH access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "terraform-ec2-sg"
+  }
+}
